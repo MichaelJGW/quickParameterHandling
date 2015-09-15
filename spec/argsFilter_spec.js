15af0dfd3ec8675f9da86e48cb53e5c7a2e7f294
ThornWinters <MichaelJWarner@hotmail.com>
2015-09-15 11:46:47 -0400

    Changed file structure and corrected file names The agrsFilter_spec.js name was changed to argsFilter.js The agrsFilter.js was moved to a lib folder so the structure is changed to project files in the root path. Test files in the spec folder. The files for the program itself is in the lib folder.

--- spec/argsFilter_spec.js
+++ spec/argsFilter_spec.js
@@ -2,8 +2,7 @@ describe('Change Directly', function(){
 	it("Default", function(){
 		var args = [];
 		var boo = function(a,boo,length){
-			arguments.defaults([5,'hello']);
-			for(arguments.length)
+			
 		}
 		expect(_.first([5, 4, 3, 2, 1])).toEqual(5);
 	});
